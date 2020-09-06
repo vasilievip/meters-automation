70332caaaadb58cccdbdd8e784340efc4bbfd40a
Ivan Vasyliev <vasilievip@gmail.com>
2020-09-06 19:36:18 +0300

    updated readme

---
+++ src/main/java/com/github/meters/metersautomation/telegram/TelegramBotVideoSender.java
@@ -0,0 +1,11 @@
+package com.github.meters.metersautomation.telegram;
+
+//https://habr.com/ru/post/432548/
+//https://dev.to/amree/a-trick-to-make-your-telegram-bot-private-5hjk
+//https://monsterdeveloper.gitbooks.io/writing-telegram-bots-on-java/content/lesson-2.-photobot.html
+//https://sarafian.github.io/low-code/2020/03/24/create-private-telegram-chatbot.html
+//https://stackoverflow.com/questions/44674075/can-i-make-my-telegram-bot-for-private-chat-only-not-groups
+//https://stackoverflow.com/questions/46015319/how-to-make-a-private-telegram-bot-accessible-only-by-its-owner?rq=1
+//https://core.telegram.org/bots/samples
+public class TelegramBotVideoSender {
+}
