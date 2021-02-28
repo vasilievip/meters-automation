16e5fb20b348f9d80452cf6437f4730952b31319
Ivan Vasyliev <vasilievip@gmail.com>
2021-02-28 21:32:47 +0200

    fixed warning message

--- src/main/java/com/github/meters/metersautomation/kyivteploenergo/CountersPage.java
+++ src/main/java/com/github/meters/metersautomation/kyivteploenergo/CountersPage.java
@@ -3,8 +3,14 @@ package com.github.meters.metersautomation.kyivteploenergo;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 public class CountersPage {
+
+    private static final int TIME_OUT_IN_SECONDS = 30;
 
     By hotWaterTextLocator = By.xpath("//div[contains(text(),'Гаряча')]");
 
@@ -12,6 +18,7 @@ public class CountersPage {
     By hotWaterNextLocator;
 
     By submitLocator = By.id("edit-next");
+    By submitBigDifferenceLocator = By.id("edit-next-big-counters");
     By successPanelLocator = By.xpath("//*[@class='success']");
 
 
@@ -61,10 +68,17 @@ public class CountersPage {
 
     public CountersPage submit(){
         webDriver.findElement(submitLocator).click();
+        List<WebElement> elements = webDriver.findElements(submitBigDifferenceLocator);
+        if(elements.size() > 0 && elements.get(0).isDisplayed()){
+            throw new IllegalStateException("Зважаючи на зазначені Вами показання, гарячої води було спожито понад 20 куб.");
+        }
         return this;
     }
 
     public String getSuccessPopupText(){
+        new WebDriverWait(webDriver, TIME_OUT_IN_SECONDS)
+                .until(ExpectedConditions.visibilityOfElementLocated(successPanelLocator));
+
         return webDriver.findElement(successPanelLocator).getAttribute("innerHTML");
     }
 }
